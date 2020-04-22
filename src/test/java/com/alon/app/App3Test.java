c2367c078dd7ee74a09c69129f6fea2268248662
dalon1 <dannelvision@gmail.com>
2020-04-21 21:34:30 -0400

    Adding maven layout plus unit testing

---
+++ src/test/java/com/alon/app/App3Test.java
@@ -0,0 +1,50 @@
+package com.alon.app;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class App3Test 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+    
+    @Test
+	public void test1() {
+		assertEquals("dannel", "dannel");
+	}
+	
+	@Test
+	public void test2() {
+		assertEquals("dannel", "dannel");
+	}
+
+	
+	@Test
+	public void test3() {
+		assertEquals("dannel", "dannel");
+	}
+
+	
+	@Test
+	public void test4() {
+		assertEquals("dannel", "dannel");
+	}
+
+	
+	@Test
+	public void test5() {
+		assertEquals("dannel", "dannel");
+	}
+
+}
